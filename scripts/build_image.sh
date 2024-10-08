f4f98911f3ea07a61ec7613b46735221aab1a57f
davidliyutong <davidliyutong@sjtu.edu.cn>
2024-10-09 00:53:15 +0800

    Fix build_image.sh

--- scripts/build_image.sh
+++ scripts/build_image.sh
@@ -2,14 +2,22 @@
 
 source scripts/docker_common.sh
 
+arch=$(uname -m)
+# if arch equals to aarch64, then set the arch to arm64, if arch equals to x86_64, then set the arch to amd64
+if [ "$arch" == "aarch64" ]; then
+  arch="arm64"
+elif [ "$arch" == "x86_64" ]; then
+  arch="amd64"
+fi
+
+
 # build the image
 echo "Building $IMAGE_REF with $BRANCH branch"
 echo "Build Args: $DOCKER_BUILD_ARGS"
-docker build $DOCKER_BUILD_ARGS . -t $IMAGE_REF -f manifests/docker/$BRANCH/Dockerfile
+docker build $DOCKER_BUILD_ARGS . -t $IMAGE_REF-$arch -f manifests/docker/$BRANCH/Dockerfile
 
 # remove dangling images
 danglingimages=$(docker images --filter "dangling=true" -q); \
 if [[ $danglingimages != "" ]]; then \
   docker rmi $(docker images --filter "dangling=true" -q); \
 fi
-
